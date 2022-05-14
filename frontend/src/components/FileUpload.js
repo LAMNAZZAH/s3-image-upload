9ca148bf6fd8e541938a30700f6be98a94d2817c
LAMNAZZAH <medlmz01@gmail.com>
2022-05-14 03:50:41 +0100

    add progress bar

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,69 +1,88 @@
 import React, { Fragment, useState } from "react";
-import Message from './Message'
-import axios from 'axios';
+import Message from "./Message";
+import axios from "axios";
+import Progress from './Progress'
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
   const [filename, setFilename] = useState("Choose file");
-  const [uploadedFile, setUploadedFile] = useState({}); 
-  const [message, setMessage] = useState()
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState();
+  const [uplaodPercentage, setuploadPercentage] = useState(0);
 
   const fileChange = (e) => {
-      e.preventDefault(); 
-      console.log(e.target.files[0]);
-      setFile(e.target.files[0])
-      console.log(`file: ${filename}`);
-      setFilename(e.target.files[0].name);
-  }
+    e.preventDefault();
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+    console.log(`file: ${filename}`);
+    setFilename(e.target.files[0].name);
+  };
 
   const onSubmit = async (e) => {
-      e.preventDefault(); 
-      const formData = new FormData(); 
-      formData.append('file', file);
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
 
-      try {
-        const res = await axios.post('http://localhost:5000/upload', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        });
+    try {
+      const res = await axios.post("http://localhost:5000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: (ProgressEvent) => {
+          setuploadPercentage(
+            parseInt(
+              Math.round((ProgressEvent.loaded * 100) / ProgressEvent.total)
+            )
+          );
+          setTimeout(() => setuploadPercentage(0), 10000)
+        },
+      });
 
-        const { fileName, filePath } = res.data; 
-        setUploadedFile({ fileName, filePath });
-        setMessage(`Uploaded file: fileName: ${fileName}, filePath: ${filePath}`);
-
-    } catch(err) {
-
-        if(err.response.status === 500) setMessage('Internal error');
-        else setMessage(`${err.response.data.msg}`);
+      const { fileName, filePath } = res.data;
+      setUploadedFile({ fileName, filePath });
+      setMessage(`Uploaded file: fileName: ${fileName}, filePath: ${filePath}`);
+    } catch (err) {
+      if (err.response.status === 500) setMessage("Internal error");
+      else setMessage(`${err.response.data.msg}`);
     }
-  }
-
+  };
 
   return (
     <Fragment>
-        { message ? <Message msg={message} /> : null }
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className="custom-file">
-          <input type="file" className="custom-file-input" id="customFile" onChange={(e) => fileChange(e)}/>
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={(e) => fileChange(e)}
+          />
           <label className="custom-file-label" htmlFor="customFile">
             {filename}
           </label>
         </div>
+        <Progress percentage={uplaodPercentage} />
         <input
           type="submit"
           value="Upload"
           className="btn btn-primary btn-block mt-4"
         />
       </form>
-      { uploadedFile ? <div className="row mt-5">
+      {uploadedFile ? (
+        <div className="row mt-5">
           <div className="col-md-6 m-auto">
-              <h3 className="text-center">
-                  {uploadedFile.fileName}
-              </h3>
-              <img style={{width: '100%'}} src={`http://localhost:5000${uploadedFile.filePath}`} alt="" />
+            <h3 className="text-center">{uploadedFile.fileName}</h3>
+            <img
+              style={{ width: "100%" }}
+              src={`http://localhost:5000${uploadedFile.filePath}`}
+              alt=""
+            />
           </div>
-      </div> : <></> }
+        </div>
+      ) : (
+        <></>
+      )}
     </Fragment>
   );
 };
---
+++ frontend/src/components/Progress.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Progress = ({ percentage }) => {
+  return (
+    <div className="progress">
+      <div
+        className="progress-bar"
+        role="progressbar"
+        aria-valuenow="0"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        style={{ width: `${percentage}%` }}
+      />
+      %{percentage}
+    </div>
+  );
+};
+
+Progress.propTypes = {
+  percentage: PropTypes.number.isRequired,
+};
+
+export default Progress;
